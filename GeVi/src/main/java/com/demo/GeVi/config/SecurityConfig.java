409a41c376c23b824fdd1677bc6b4472c3905a41
Felipe García <mac27bross@hotmail.com>
2025-08-08 11:45:33 -0600

    PEQUEÑAS CORRECCIONES

--- GeVi/src/main/java/com/demo/GeVi/config/SecurityConfig.java
+++ GeVi/src/main/java/com/demo/GeVi/config/SecurityConfig.java
@@ -75,7 +75,10 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of(
                 "http://localhost:3000",
-                "http://172.20.10.2:3000"));
+                "http://172.20.10.2:3000",
+                "http://192.168.1.64:3000",
+                "http://10.16.118.223:3000",
+                "http://192.168.1.64:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Permite el uso de cookies o tokens con CORS
